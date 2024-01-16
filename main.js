7f761490a87bc612b13dc7e622f53fec401bfd74
agnesvarga1 <agikolyok@gmail.com>
2024-01-16 15:39:56 +0100

    play btn fn + activeCell on click

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const gridHtml = document.querySelector("#grid");
-
+const playBtn = document.querySelector(".play-btn");
 let cellPerLine = 10;
 
-for (let i = 1; i <= cellPerLine ** 2; i++) {
-  let cellHtml = document.createElement("div");
-  cellHtml.classList.add("cell");
-  cellHtml.innerHTML = `<span>${i}</span>`;
-  gridHtml.append(cellHtml);
+function startGame() {
+  for (let i = 1; i <= cellPerLine ** 2; i++) {
+    let cellHtml = document.createElement("div");
+    cellHtml.classList.add("cell");
+    cellHtml.innerHTML = `<span>${i}</span>`;
+
+    cellHtml.addEventListener("click", function () {
+      this.classList.add("active");
+      let cellNum = this.querySelector("span").innerText;
+      console.log(cellNum);
+    });
+    gridHtml.append(cellHtml);
+  }
 }
+
+playBtn.addEventListener("click", () => {
+  startGame();
+});
