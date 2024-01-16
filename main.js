6515780f8a9b6ea52517624f389a75618fb8a953
agnesvarga1 <agikolyok@gmail.com>
2024-01-16 15:22:22 +0100

    add grid with nums

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("haho");
+const gridHtml = document.querySelector("#grid");
+
+let cellPerLine = 10;
+
+for (let i = 1; i <= cellPerLine ** 2; i++) {
+  let cellHtml = document.createElement("div");
+  cellHtml.classList.add("cell");
+  cellHtml.innerHTML = `<span>${i}</span>`;
+  gridHtml.append(cellHtml);
+}
